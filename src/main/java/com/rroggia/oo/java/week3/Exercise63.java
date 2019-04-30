93a76f0deb9a967268fdb544ce676d52266cb848
RRoggia <renanzr@gmail.com>
2019-04-30 13:01:10 -0300

    add exercise 63

---
+++ src/main/java/com/rroggia/oo/java/week3/Exercise63.java
@@ -0,0 +1,13 @@
+package com.rroggia.oo.java.week3;
+
+import java.util.ArrayList;
+
+public class Exercise63 {
+
+	public static int sum(ArrayList<Integer> list) {
+		// write your code here
+
+		return 0;
+	}
+
+}
---
+++ src/test/java/com/rroggia/oo/java/solution/week3/SolutionExercise63Test.java
@@ -0,0 +1,22 @@
+package com.rroggia.oo.java.solution.week3;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class SolutionExercise63Test {
+
+	@Test
+	public void sumArrayNumbers() {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		numbers.add(3);
+		numbers.add(2);
+		numbers.add(7);
+		numbers.add(2);
+
+		assertEquals(14, SolutionExercise63.sum(numbers));
+	}
+
+}
