28668dc253ae8a7c8ddf5593f1ab3711045752d5
RRoggia <renanzr@gmail.com>
2019-04-30 17:27:36 -0300

    add implementation for exercise 69

--- src/main/java/com/rroggia/oo/java/part1/solution/week3/SolutionExercise69.java
+++ src/main/java/com/rroggia/oo/java/part1/solution/week3/SolutionExercise69.java
@@ -3,8 +3,7 @@ package com.rroggia.oo.java.part1.solution.week3;
 public class SolutionExercise69 {
 
 	public static boolean palindrome(String text) {
-		// missing implementation of exercise 53
-		return true;
+		return text.equals(SolutionExercise56.reverse(text));
 	}
 
 }
