8e0acfebd7c297663daa7e72ba52efdba5767768
i840973 <renan.roggia@sap.com>
2019-04-30 11:20:58 -0300

    add exercise46

---
+++ src/test/java/com/rroggia/oo/java/solution/week3/SolutionExercise46Test.java
@@ -0,0 +1,14 @@
+package com.rroggia.oo.java.solution.week3;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionExercise46Test {
+
+	@Test
+	public void average() {
+		assertEquals(SolutionExercise46.average(4, 3, 6, 1), 3.5, 1.0);
+	}
+
+}
---
+++ src/test/java/com/rroggia/oo/java/week3/Exercise46Test.java
@@ -0,0 +1,15 @@
+package com.rroggia.oo.java.week3;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class Exercise46Test {
+	
+	@Test
+	public void average() {
+		assertEquals(Exercise46.average(4, 3, 6, 1), 3.5, 1.0);
+	}
+
+}
