9c9de30b98109f948647df1bb67772ffca47dc67
RRoggia <renanzr@gmail.com>
2019-04-30 16:52:36 -0300

    add exercise 48

---
+++ src/main/java/com/rroggia/oo/java/part1/solution/week3/SolutionExercise48.java
@@ -0,0 +1,9 @@
+package com.rroggia.oo.java.part1.solution.week3;
+
+public class SolutionExercise48 {
+
+	public static char firstCharacter(String text) {
+		return text.charAt(0);
+	}
+
+}
---
+++ src/test/java/com/rroggia/oo/java/part1/solution/week3/SolutionExercise48Test.java
@@ -0,0 +1,14 @@
+package com.rroggia.oo.java.part1.solution.week3;
+
+import org.junit.Test;
+
+import com.rroggia.oo.java.part1.week3.Exercise48Test;
+
+public class SolutionExercise48Test {
+
+	@Test
+	public void firstCharacter() {
+		Exercise48Test.testImplementationOfFirstCharacter(SolutionExercise48.class);
+	}
+
+}
