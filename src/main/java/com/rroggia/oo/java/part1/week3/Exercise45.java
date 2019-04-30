0e1500e6a11753842be95d93a0c21c70f6ae6954
RRoggia <renanzr@gmail.com>
2019-04-30 16:07:43 -0300

    remove implementation from exercise45

--- src/main/java/com/rroggia/oo/java/part1/week3/Exercise45.java
+++ src/main/java/com/rroggia/oo/java/part1/week3/Exercise45.java
@@ -2,10 +2,4 @@ package com.rroggia.oo.java.part1.week3;
 
 public class Exercise45 {
 
-	public static int greatest(int number1, int number2, int number3) {
-		// write your code here
-
-		throw new RuntimeException("You have to implement this method. Don't forget to remove this statement.");
-	}
-
 }
