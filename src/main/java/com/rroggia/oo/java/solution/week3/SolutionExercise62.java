52cd44746b125bdcd816a97ec8b88650a2bd06b4
RRoggia <renanzr@gmail.com>
2019-04-30 12:48:57 -0300

    add exercise 62

---
+++ src/main/java/com/rroggia/oo/java/solution/week3/SolutionExercise62.java
@@ -0,0 +1,11 @@
+package com.rroggia.oo.java.solution.week3;
+
+import java.util.ArrayList;
+
+public class SolutionExercise62 {
+
+	public static void removeLast(ArrayList<String> list) {
+		list.remove(list.size() - 1);
+	}
+
+}
---
+++ src/main/java/com/rroggia/oo/java/week3/Exercise62.java
@@ -0,0 +1,11 @@
+package com.rroggia.oo.java.week3;
+
+import java.util.ArrayList;
+
+public class Exercise62 {
+
+	public static void removeLast(ArrayList<String> brothers) {
+
+	}
+
+}
