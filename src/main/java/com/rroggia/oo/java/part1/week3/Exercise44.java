c9a38b720eab693d3ee7c1985b4274603e3c57db
RRoggia <renanzr@gmail.com>
2019-04-30 15:54:59 -0300

    change exercise 44 tests to use reflective api

--- src/main/java/com/rroggia/oo/java/part1/week3/Exercise44.java
+++ src/main/java/com/rroggia/oo/java/part1/week3/Exercise44.java
@@ -2,12 +2,4 @@ package com.rroggia.oo.java.part1.week3;
 
 public class Exercise44 {
 
-	public static int least(int number1, int number2) {
-		// write program code here
-		// do not print anything inside the method
-
-		// method needs a return in the end
-		throw new RuntimeException("You have to implement this method. Don't forget to remove this statement.");
-	}
-
 }
--- src/test/java/com/rroggia/oo/java/part1/week3/Exercise44Test.java
+++ src/test/java/com/rroggia/oo/java/part1/week3/Exercise44Test.java
@@ -1,26 +1,50 @@
 package com.rroggia.oo.java.part1.week3;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import org.junit.Test;
-
-import com.rroggia.oo.java.part1.week3.Exercise44;
 
 public class Exercise44Test {
 
 	@Test
 	public void firstParameterIsTheLeast() {
-		assertEquals(1, Exercise44.least(1, 8));
+		Exercise44Test.least(1, 1, 8);
 	}
 
 	@Test
 	public void secondParameterIsTheLeast() {
-		assertEquals(3, Exercise44.least(8, 3));
+		Exercise44Test.least(3, 8, 3);
 	}
 
 	@Test
 	public void parametersAreEqual() {
-		assertEquals(4, Exercise44.least(4, 4));
+		Exercise44Test.least(4, 4, 4);
+	}
+
+	private static void least(int expectedResult, int number1, int number2) {
+		try {
+			Method sumInstanceMethod = Exercise44.class.getMethod("least", int.class, int.class);
+			Object invokeResult = sumInstanceMethod.invoke(null, number1, number2);
+			int result;
+
+			if (invokeResult != null)
+				result = (int) invokeResult;
+			else
+				throw new IllegalArgumentException();
+
+			assertEquals(expectedResult, result);
+
+		} catch (NoSuchMethodException e) {
+			fail("Create the method least, which returns the least of the numbers given as parameters.");
+		} catch (ClassCastException | NullPointerException | IllegalAccessException | IllegalArgumentException e) {
+			fail("Your method must follow: public static int least(int number1, int number2)");
+		} catch (InvocationTargetException e) {
+			fail(e.getTargetException().getMessage());
+		}
 	}
 
 }
